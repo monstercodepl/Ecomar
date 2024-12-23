b2a320231728d79171e1f5c8cd69adc7823872cd
jzacios <40343080+jzacios@users.noreply.github.com>
2024-12-23 14:09:05 +0100

    fix logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+<script src='fullcalendar/core/locales/pl.global.js'></script>
     <script>
 
       document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
+          locale: 'pl',
           initialView: 'dayGridMonth',
           headerToolbar: {
             left: 'prev,next',
             center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
           }
         });
         calendar.render();
