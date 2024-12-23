08697a18eb7248f15003aef3f0917461e01f72d9
Mariusz Głuch <hi@monstercode.pl>
2024-12-23 19:18:01 +0100

    Full calendar update

--- resources/views/layouts/footers/auth/footer.blade.php
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -2,31 +2,15 @@
     <div class="container-fluid">
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
-                <div class="copyright text-center text-sm text-muted text-lg-start">
+               
+            </div>
+            <div class="col-lg-6">
+            <div class="copyright text-center text-sm text-muted text-lg-start">
                     © <script>
                         document.write(new Date().getFullYear())
                     </script>
-                     <a style="color: #252f40;" href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Monstercode</a>
+                     <a style="color: #252f40;" href="https://www.monstercode.pl" class="font-weight-bold ml-1" target="_blank">Monster Code</a>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                    <li class="nav-item">
-                        <a href="" class="nav-link text-muted" target="_blank">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-link text-muted" target="_blank">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-link text-muted" target="_blank">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-link text-muted" target="_blank">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-link pe-0 text-muted" target="_blank">Link</a>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
