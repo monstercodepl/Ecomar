39ca4b06de1fb10f12caaa360cfb81e240a0f9f5
jzacios <40343080+jzacios@users.noreply.github.com>
2025-02-02 21:39:44 +0100

    fix e0mail

--- resources/views/mail/job/finished.blade.php
+++ resources/views/mail/job/finished.blade.php
@@ -1,4 +1,4 @@
-Dziękujemy za skorzystanie z usługi asemizacyjnej. <br><br>
+Dziękujemy za skorzystanie z usługi asenizacyjnej. <br><br>
 Data: {{$job->updated_at}} <br><br>
 Numer konta do płatności: 62 1090 1450 0000 0001 2603 4343 <br><br>
 Tytuł: {{$job->address->adres ?? ''}} {{$job->address->numer ?? ''}}, {{$job->address->miasto}} <br><br>
