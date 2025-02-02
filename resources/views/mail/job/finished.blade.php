120c7669bd564a3ef13f72733c888fee85eddae8
Mariusz Głuch <hi@monstercode.pl>
2025-02-02 11:08:19 +0100

    FiX PDF

--- resources/views/mail/job/finished.blade.php
+++ resources/views/mail/job/finished.blade.php
@@ -1,4 +1,9 @@
 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<style>
+body{font-family: "DejaVu Sans", sans-serif;}
+</style>
+<body>
 <div style="width: 500px;">
  <div style="width: 198px; text-align: center; float: left; padding-top: 19px; padding-bottom: 19px; border: 1px solid black">
    <b>ECO-MAR</b><br>
@@ -27,4 +32,5 @@
 </p>
  </div>
 </div>
+</body>
 
