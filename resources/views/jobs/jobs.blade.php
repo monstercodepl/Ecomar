1c52d28d13ed76cec65ecdd918c1a3b8b4ba10b1
Mariusz Głuch <hi@monstercode.pl>
2025-02-05 21:19:33 +0100

    confirmation delete

--- resources/views/jobs/jobs.blade.php
+++ resources/views/jobs/jobs.blade.php
@@ -71,12 +71,33 @@
                                             <p class="text-xs font-weight-bold mb-0">{{$job->status}}</p>
                                         </td>
                                         <td class="text-center">
-                                            <form method="POST" action="job/delete">
-                                                @csrf
-                                                <input type="hidden" name="job_id" value="{{$job->id}}">
-                                                <button type="submit" class="btn bg-danger text-white btn-md mt-4 mb-1">Usuń</button>
-                                            </form>
-                                            <a href="{{ route('job', array('id' => $job->id)) }}"><button class="btn bg-warning text-white btn-md mb-4">Edytuj</button></a>
+                                            <button type="button" class="btn bg-danger text-white btn-md" data-bs-toggle="modal" data-bs-target="#deleteModal{{$job->id}}">
+                                                 Usuń
+                                            </button>
+
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="deleteModal{{$job->id}}" tabindex="-1" aria-labelledby="deleteModalLabel{{$job->id}}" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="deleteModalLabel{{$job->id}}">Potwierdzenie usunięcia</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Zamknij"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Czy na pewno chcesz usunąć to zlecenie?
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Anuluj</button>
+                                                            <form method="POST" action="job/delete">
+                                                                @csrf
+                                                                <input type="hidden" name="job_id" value="{{$job->id}}">
+                                                                <button type="submit" class="btn btn-danger">Usuń</button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <a href="{{ route('job', array('id' => $job->id)) }}"><button class="btn bg-wargning btn-md">Edytuj</button></a>
                                         </td>
                                     </tr>
                                 @endforeach
