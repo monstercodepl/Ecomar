01024f924354931d12e324c9237ccec4403b4e55
Mariusz Głuch <hi@monstercode.pl>
2025-02-06 19:52:56 +0100

    update daily list

--- resources/views/jobs/report.blade.php
+++ resources/views/jobs/report.blade.php
@@ -1,35 +1,54 @@
 <html>
-<style>
-table, th, td {
-  border:1px solid black;
-}
-</style>
-<body>
-<table>
-    Data: {{$date}}
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Ulica</th>
-            <th>Miejscowosc</th>
-            <th>Wielkosc zbiornika</th>
-            <th>Wywiezione m3</th>
-            <th  style="padding-left: 20px; padding-right: 20px;">Zlewnia</th>
-            <th>Zaplacono</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($jobs as $job)
-            <tr>
-                <th>{{$job->id}}</th>
-                <th>{{$job->address->adres ?? ''}} {{$job->address->numer ?? ''}}</th>
-                <th>{{$job->address->miasto ?? ''}}</th>
-                <th>{{$job->address->zbiornik ?? ''}}</th>
-                <th></th>
-                <th></th>
-                <th></th>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>Lista Zadań</title>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+                border-collapse: collapse;
+                padding: 5px;
+            }
+            caption {
+                font-weight: bold;
+                margin-bottom: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        @php
+            $sortedJobs = $jobs->sortBy(function($job) {
+                return $job->address->adres ?? '';
+            });
+        @endphp
+
+        <table>
+            <caption>Data: {{ $date }}</caption>
+            <thead>
+                <tr>
+                    <th>LP</th>
+                    <th>ID</th>
+                    <th>Ulica</th>
+                    <th>Miejscowość</th>
+                    <th>Wielkość zbiornika</th>
+                    <th>Wywiezione m3</th>
+                    <th style="padding-left: 20px; padding-right: 20px;">Zlewnia</th>
+                    <th>Zapłacono</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($sortedJobs as $job)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $job->id }}</td>
+                        <td>{{ $job->address->adres ?? '' }} {{ $job->address->numer ?? '' }}</td>
+                        <td>{{ $job->address->miasto ?? '' }}</td>
+                        <td>{{ $job->address->zbiornik ?? '' }}</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </body>
+</html>
